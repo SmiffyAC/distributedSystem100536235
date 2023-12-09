e84f630598568aeb25861d47eb1f001e74e53170
Aaron Smith <56254617+SmiffyAC@users.noreply.github.com>
2023-12-09 13:15:19 +0000

    Added node code that can connect to bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -28,20 +28,20 @@ class BootstrapServer:
                 # Print connected client information
                 print(f"Connected to {client.getpeername()}")
                 # Handle each client connection in a new thread
-                # threading.Thread(target=self.handle_node, args=(client,)).start()
+                threading.Thread(target=self.handle_node, args=(client,)).start()
 
-    # def handle_node(self, client):
-    #     try:
-    #         # Receive data from node
-    #         data = client.recv(1024).decode('utf-8')
-    #         # Decode data to JSON
-    #         node_info = json.loads(data)
-    #         # Register node information
-    #         self.nodes[node_info['name']] = (node_info['ip'], node_info['port'])
-    #         print(f"Registered node: {node_info}")
-    #     finally:
-    #         # Close the connection
-    #         client.close()
+    def handle_node(self, client):
+        try:
+            # Receive data from node
+            data = client.recv(1024).decode('utf-8')
+            # Decode data to JSON
+            node_info = json.loads(data)
+            # Register node information
+            self.nodes[node_info['name']] = (node_info['ip'], node_info['port'])
+            print(f"Registered node: {node_info}")
+        finally:
+            # Close the connection
+            client.close()
 
 
 if __name__ == '__main__':
---
+++ node.py
@@ -0,0 +1,28 @@
+import socket
+import json
+
+class Node:
+    def __init__(self, name, host='localhost', port=9000):
+        # Initialize the client with a name, host, and port
+        node_name = socket.gethostname()
+        node_ip = socket.gethostbyname(node_name)
+        self.name = name
+        self.host = node_ip
+        self.port = port
+
+    def connect_to_bootstrap(self, bootstrap_host, bootstrap_port):
+        # Connect to the Bootstrap Server
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((bootstrap_host, bootstrap_port))
+            # Prepare the client information as JSON
+            client_info = {"name": self.name, "ip": self.host, "port": self.port}
+            # Send the client information to the Bootstrap Server
+            sock.sendall(json.dumps(client_info).encode('utf-8'))
+            print(f"Connected to Bootstrap Server and sent info: {client_info}")
+
+if __name__ == '__main__':
+    # Create a client instance with a unique name
+    client = Node(name="node")
+    # Connect the client to the Bootstrap Server
+    bootstrap_ip = '192.168.56.1'
+    client.connect_to_bootstrap(bootstrap_ip, 8000)
